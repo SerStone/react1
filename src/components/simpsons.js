583d55641d2f07384cab53d86ffcac72b5e471f0
SerStone <mishagoal752@gmail.com>
2022-09-12 16:43:09 +0300

    Go React

---
+++ src/components/simpsons.js
@@ -0,0 +1,12 @@
+function Simpsons(props){
+    let {itemName,pic} = props
+    let  classNameItem = 'card';
+    return(
+        <div className={classNameItem}>
+            <h2>{itemName}</h2>
+            <img src={pic} alt={'Simpsons'}/>
+        </div>
+    );
+}
+
+export default Simpsons;
