5106f00ef5f24c4a99261ff249d552f0f53802fa
SerStone <mishagoal752@gmail.com>
2022-09-19 13:11:20 +0300

    Go React2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Users from "./componenets/Users/Users";
 import {Posts} from "./componenets/Posts/Posts";
-
 import {getPostsAxios} from './services/axiosGetter'
+
 import {useState} from "react";
 
 function App() {
