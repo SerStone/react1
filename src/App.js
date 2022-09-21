8de3669ae36d1249aacc24b75ef6f6d7ebdcdd6d
SerStone <mishagoal752@gmail.com>
2022-09-21 16:20:42 +0300

    Go React2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {Cars} from "./components";
 
 function App() {
   // eslint-disable-next-line no-unused-vars
   let  classNameItem = 'card';
   return (
     <div className="App">
+
+
+    <Cars/>
+
 
     </div>
   );
--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,10 +1,52 @@
+import {useForm} from "react-hook-form";
 
-const CarForm = () => {
+import {joiResolver} from "@hookform/resolvers/joi";
+
+import {carValidator} from "../../validators";
+
+import {carService} from "../../services";
+import {useEffect} from "react";
+
+const CarForm = ({setCars,updateCar}) => {
+    const {register,handleSubmit,reset,formState:{isValid,errors},setValue} = useForm({
+        resolver:joiResolver(carValidator),
+        mode:'all'});
+
+    const {id,model,price,year} = updateCar
+
+    const submit = async (car) =>{
+        if (id){
+            const {data} = await carService.updateById(id,car);
+            setCars(cars =>[...cars,data])
+        }
+        else {
+
+            const {data} = await carService.create(car);
+            setCars(cars =>[...cars,data])
+            reset()
+        }
+    };
+
+    useEffect(()=>{
+        setValue('model',model);
+        setValue('price',price);
+        setValue('year',year);
+    },[id])
+
+
+
     return (
-        <div>
+        <form onSubmit={handleSubmit(submit)} onChange={()=>console.log(errors)}>
+            <input type="text" placeholder={'model'} {...register('model')}/>
+            {errors.model&&<span>{errors.model.message}</span>}
+            <input type="text" placeholder={'price'} {...register('price', {valueAsNumber:true})}/>
+            {errors.price&&<span>{errors.price.message}</span>}
+            <input type="text" placeholder={'year'} {...register('year',{valueAsNumber:true})}/>
+            {errors.year&&<span>{errors.year.message}</span>}
 
-        </div>
+            <button disabled={!isValid}>{id ?'Update':'Save'}</button>
+        </form>
     );
 };
 
-export default CarForm;
+export {CarForm};
