84e21d77be4514e903586b0662ca730df29c5e52
SerStone <mishagoal752@gmail.com>
2022-09-25 12:44:08 +0300

    Go React3

--- src/components/AboutComments/Comment/Comment.js
+++ src/components/AboutComments/Comment/Comment.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Comment = ({comment}) => {
     return (
         <div>
-            <h3>PostId: {comment.postId} Id: {comment.id} Title: {comment.title}</h3>
+            <h3>PostId: {comment.postId} Id: {comment.id} Title:</h3>
+            <Link to={`comments/${comment.id}`}><h3>{comment.title}</h3></Link>
             <p>Body: {comment.body}</p>
         </div>
     );
