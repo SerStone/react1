0a069ec465879b1d9bac3b0df05da14af83cb83a
SerStone <mishagoal752@gmail.com>
2022-09-25 17:59:40 +0300

    Go React3

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -9,18 +9,18 @@ import {Car} from "../Car/Car";
 
 const Cars = () => {
 const [cars,setCars] = useState([]);
-const [carUpdate,setUpdate] =useState({});
+const [carUpdate,setUpdate] =useState(null);
 
 useEffect(()=>{
     carService.getAll().then(({data})=>setCars(data))
-},[cars])
+},[])
     return (
         <div>
             <CarForm setCars={setCars} carUpdate={carUpdate} setUpdate={setUpdate}/>
             <hr/>
             <div className={css.Cars}>
 
-            {cars.map((car,index)=><Car key={index} setCars={setCars} car={car} setUpdate={setUpdate}/>)}
+            {cars.map((car,index)=><Car key={car.id} setCars={setCars} car={car} setUpdate={setUpdate}/>)}
             </div>
         </div>
     );
