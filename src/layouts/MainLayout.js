d6d801136b3389ed926547da16ebc3c89d25117e
SerStone <mishagoal752@gmail.com>
2022-10-05 16:31:25 +0300

    Go React3

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouterLink,Link, Route, Routes} from "react-router-dom";
 
 const MainLayout = () => {
 
@@ -6,7 +7,20 @@ const MainLayout = () => {
 
     return (
         <div>
+            {/*<ul className={'Navbar'}>*/}
+            {/*    <li><Link to={'/'}>Home Page</Link></li>*/}
+            {/*    <li><Link to={'/todos'}>Todos</Link></li>*/}
+            {/*    <li><Link to={'/albums'}>Albums </Link></li>*/}
+            {/*    <li><Link to={'/comments'}>Comments</Link></li>*/}
+            {/*</ul>*/}
 
+            {/*<Routes>*/}
+            {/*    <Route path={'/'} element={<Home/>}/>*/}
+            {/*    <Route path={'/todos'} element={<Todos/>}/>*/}
+            {/*    <Route path={'/albums'} element={<Albums/>}/>*/}
+            {/*    <Route path={'/comments'} element={<Comments/>}/>*/}
+            {/*    <Route path={'/comments/:id'} element={<Posts/>}/>*/}
+            {/*</Routes>*/}
         </div>
     );
 };
