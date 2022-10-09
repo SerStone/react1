f81f208b6ae966b0d25b7d362085eddc2ac77247
SerStone <mishagoal752@gmail.com>
2022-10-10 00:20:10 +0300

    Go React redux

--- src/componenets/About-Posts/Post/Post.js
+++ src/componenets/About-Posts/Post/Post.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {useDispatch} from "react-redux";
+
+import css from './Post.module.css'
 import {postActions} from "../../../redux";
 
 const Post = ({post}) => {
@@ -8,13 +10,15 @@ const Post = ({post}) => {
     const {id,title,body} = post;
 
     return (
-        <div>
+        <div className={css.post}>
             id: {id}<br/>
             title:{title}<br/>
             body:{body}<br/>
-            <button onClick={()=>dispatch(postActions.setCurrentPost(post))}>setCurrentPost</button><br/>
-            <button onClick={()=>dispatch(postActions.getById({id}))}>getById</button><br/>
-            <button onClick={()=>dispatch(postActions.deleteById(id))}>deleteById</button>
+            <div className={css.postButtons}>
+            <button className={css.buttonPost} onClick={()=>dispatch(postActions.setCurrentPost(post))}>setCurrentPost</button><br/>
+            <button className={css.buttonPost} onClick={()=>dispatch(postActions.getById({id}))}>getById</button><br/>
+            <button className={css.buttonPost} onClick={()=>dispatch(postActions.deleteById(id))}>deleteById</button>
+            </div>
         </div>
     );
 };
