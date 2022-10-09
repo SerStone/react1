651d9f58c98427b47878726da38f22a04a35cd1b
SerStone <mishagoal752@gmail.com>
2022-10-10 01:05:13 +0300

    Go React redux

--- src/componenets/Header/Header.js
+++ src/componenets/Header/Header.js
@@ -1,14 +1,20 @@
 import css from './Header.module.css'
 import {useSelector} from "react-redux";
+import {Link} from "react-router-dom";
+import React from "react";
 
 const Header = () => {
     const {currentUser} = useSelector(state => state.userReducer);
     const {currentPost} = useSelector(state => state.postReducer);
     return (
-        <div className={css.Header}>
+
+            <ul className={css.Header}>
+                <li><Link to={'/users'}>Users</Link></li>
+                <li><Link to={'/posts'}>Posts </Link></li>
             {currentUser&&currentUser.name}
             {currentPost&&currentPost.title}
-        </div>
+            </ul>
+
     );
 };
 
